c34ccaf541de55ceeaa4b888844231e0a59cc9a1
smd tech <smdtechdev@gmail.com>
2021-05-09 17:44:19 +0100

    added func

--- New Python/htmlstuff.py
+++ New Python/htmlstuff.py
@@ -12,5 +12,14 @@ for chunk in res.iter_content(100000):
     myfile.write(chunk)
     
 print(myfile)
-myfile.close()    
+myfile.close()
 
+def aboutSite(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#whatWeDo > div > div > div > p:nth-child(1)')   
+    response = elems[0].text.strip()
+    return response
+
+print(aboutSite('http://www.smd.tech'))
