e20e1a5a2c917d6ef51d435c952e80221b779d57
smd tech <smdtechdev@gmail.com>
2022-01-23 16:36:41 +0000

    web

--- Web/app.py
+++ Web/app.py
@@ -1,20 +1,59 @@
-from tarfile import FIFOTYPE
-from flask import request
-from flask import Flask, flash, render_template, url_for
+from flask import redirect, request, Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = "12345"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    flash("What's your name?")
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error adding task'    
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_del = Todo.query.get_or_404(id)
+    try:
+            db.session.delete(task_del)
+            db.session.commit()
+            return redirect('/')
+    except:
+            return 'Cannot Delete'    
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == ['POST']:
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error updating task'
+    
+    else:
+         return render_template('update.html', task=task)
+    
 if __name__ == "__main__":
     app.run(debug=True)
-    
-@app.route('/greet', methods=['POST'])
-def greet():
-    flash("Hi " + str(request.form['name_input']) + ", great to see you")
-    return render_template('index.html')
